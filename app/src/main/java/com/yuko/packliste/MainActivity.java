38ec1c65e85b4b6948b469cdcbde95be9ef55b93
Julia Dondorff <jkoch@google.com>
2020-12-03 17:16:37 +0100

    Returning to the MainActivity through the back button now updates the counts.

--- app/src/main/java/com/yuko/packliste/MainActivity.java
+++ app/src/main/java/com/yuko/packliste/MainActivity.java
@@ -53,6 +53,13 @@ public class MainActivity extends AppCompatActivity implements AddItemDialogFrag
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.clear();
+        adapter.addAll(ioList.getCategoryList());
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
